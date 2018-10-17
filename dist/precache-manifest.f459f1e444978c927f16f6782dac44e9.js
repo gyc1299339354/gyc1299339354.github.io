d1ded30f0859b9dc1e2b159ba210bc2a515bd54b
Neo Geng <1299339354@qq.com>
2018-10-17 11:37:51 +0800

    u: gitignore dist allowed

---
+++ dist/precache-manifest.f459f1e444978c927f16f6782dac44e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dist/robots.txt"
+  },
+  {
+    "revision": "03bc9589e0e3f412e2c3",
+    "url": "/dist/js/chunk-vendors.3c9ef75b.js"
+  },
+  {
+    "revision": "9db7192d9f9c7d49460b",
+    "url": "/dist/js/app.1934d489.js"
+  },
+  {
+    "revision": "b13b2f416ad69cbc267272630ec54051",
+    "url": "/dist/index.html"
+  },
+  {
+    "revision": "03bc9589e0e3f412e2c3",
+    "url": "/dist/css/chunk-vendors.810a7293.css"
+  },
+  {
+    "revision": "9db7192d9f9c7d49460b",
+    "url": "/dist/css/app.38d48424.css"
+  }
+];
